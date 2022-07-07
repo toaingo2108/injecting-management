ac2f8a7b961e92705f8204684ea7c0985b7e3c3a
toaingo2108 <nqtoai2181@gmail.com>
2022-07-07 14:38:03 +0700

    edit constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,4 +1,3 @@
 export const apiUrl =
-  process.env.NODE_ENV !== 'production'
-    ? 'http://localhost:5000/api'
-    : 'https://immense-scrubland-11716.herokuapp.com/api'
+  //  'http://localhost:5000/api'
+  'https://immense-scrubland-11716.herokuapp.com/api'
